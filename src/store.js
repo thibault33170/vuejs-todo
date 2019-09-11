963cbc9f3a620d9627f112cd5bf06cb332ed09b5
thibault33170 <thibaultfaivre40@gmail.com>
2019-09-11 15:36:02 +0200

    store logic

--- src/store.js
+++ src/store.js
@@ -5,12 +5,38 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    todos: [
+      {
+        id: 1,
+        title: 'title 1',
+        description: 'description 1',
+        category: 'category 1',
+        status: 'new',
+        date: '2019-10-10 16:00:00'
+      },
+      {
+        id: 2,
+        title: 'title 2',
+        description: 'description 2',
+        category: 'category 2',
+        status: 'new',
+        date: '2019-10-10 16:00:00'
+      }
+    ]
   },
   mutations: {
-
+    createTodo(state, todo) {
+      state.todos.push(todo);
+    }
   },
   actions: {
-
+    createTodo(context, todo) {
+      context.commit('createTodo', todo);
+    }
+  },
+  getters: {
+    todos(state) {
+      return state.todos;
+    }
   }
 })
