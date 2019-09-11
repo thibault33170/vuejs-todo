d34f2f3ce7b9226ef36a7b9d729dd6f69e067979
thibault33170 <thibaultfaivre40@gmail.com>
2019-09-11 15:35:48 +0200

    dirty todo create page

---
+++ src/views/CreateTodo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="createTodo">
+    Create a new Todo
+
+    <input type="text" v-model="todo.title">
+    <textarea v-model="todo.description"></textarea>
+
+    <button @click="create">Create</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateTodo',
+  data() {
+    return {
+      todo: {
+        id: this.generateUid(),
+        title: '',
+        description: '',
+        category: '',
+        status: '',
+        date: ''
+      },
+    }
+  },
+  methods: {
+    create() {
+      this.$store.dispatch('createTodo', this.todo);
+
+      this.$router.push({name: 'todosList'})
+    },
+    generateUid() {
+      return Math.floor(Math.random() * Math.floor(999999));
+    }
+  }
+}
+</script>
