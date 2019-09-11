ec3bf1fcc508e6b2ef4895d54a266270e59fdb82
thibault33170 <thibaultfaivre40@gmail.com>
2019-09-11 15:35:36 +0200

    dirty todo listing

---
+++ src/views/TodosList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="todos">
+    <h3>All my awesome todos</h3>
+    
+    <button @click="goToCreate">New</button>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.title }}
+        {{ todo.description }}
+        {{ todo.category }}
+        {{ todo.status }}
+        {{ todo.date }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: []
+    }
+  },
+  mounted() {
+    this.todos = this.$store.getters.todos
+  },
+  methods: {
+    goToCreate() {
+      this.$router.push({path: 'newTodo'})
+    }
+  }
+}
+</script>
