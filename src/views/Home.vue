7d0492b6d133b6977d8cd18a2fa7397dec8655b1
thibault33170 <thibaultfaivre40@gmail.com>
2019-09-11 15:34:36 +0200

    dirty landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Welcome to my awesome VueJS Todolist !  
+    <router-link to="/todos">Click here to check that :)</router-link>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
