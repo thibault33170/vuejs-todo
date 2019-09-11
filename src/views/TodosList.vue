2c24517beb5af4637bb053115818400b9bc1c0f6
thibault33170 <thibaultfaivre40@gmail.com>
2019-09-11 17:03:02 +0200

    category and state filters

--- src/views/TodosList.vue
+++ src/views/TodosList.vue
@@ -12,6 +12,27 @@
         type="text"
         v-model="search"
       >
+
+      <label>Filters</label>
+      <select v-model="filters.category">
+        <option
+          v-for="category in categories"
+          :key="category.id"
+          :value="category.name"
+        >
+          {{ category.name }}
+        </option>
+      </select>
+
+      <select v-model="filters.state">
+        <option
+          v-for="state in states"
+          :key="state.id"
+          :value="state.name"
+        >
+          {{ state.name }}
+        </option>
+      </select>
     </div>
   
     <ul>
@@ -36,12 +57,20 @@
 export default {
   data() {
     return {
+      filters: {
+        category: '',
+        state: ''
+      },
+      categories: [],
+      states: [],
       todos: [],
       search:''
     }
   },
   mounted() {
     this.todos = this.$store.getters.getTodos
+    this.categories = this.$store.getters.getCategories
+    this.states = this.$store.getters.getStates
   },
   methods: {
     goToCreate() {
@@ -54,15 +83,26 @@ export default {
   computed: {
     filteredTodos() {
       return this.todos.filter((todo) => {
-        if (this.search === '') {
+        if (this.search === '' && this.filters.category === '') {
           return true
         }
 
-        if(todo.title.toLowerCase().includes(this.search.toLowerCase()) || todo.description.toLowerCase().includes(this.search.toLowerCase())) {
-          return true
+        let searchFlag = true;
+        let filtersFlag = true;
+
+        if (this.search !== '') {
+          searchFlag = todo.title.toLowerCase().includes(this.search.toLowerCase()) || todo.description.toLowerCase().includes(this.search.toLowerCase())
         }
 
-        return false
+        if(this.filters.category !== '') {
+          filtersFlag = todo.category === this.filters.category
+        }
+
+        if (this.filters.state !== '') {
+          filtersFlag = filtersFlag && (todo.state === this.filters.state)
+        }
+
+        return filtersFlag && searchFlag
       })
     }
   }
