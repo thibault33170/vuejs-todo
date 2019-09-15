a0555b7a2b54b96eef502a6662bc9256c4836b9e
thibault33170 <thibaultfaivre40@gmail.com>
2019-09-15 21:29:20 +0200

    creation form styling

--- src/views/CreateTodo.vue
+++ src/views/CreateTodo.vue
@@ -1,24 +1,41 @@
 <template>
   <div class="createTodo">
-    <h3> New Todo </h3>
+    <h3> Todo create</h3>
 
-    <form>
-      <div>
-        <label> Title </label>
-        <input
-          type="text"
+    <b-form>
+      <b-form-group
+        id="input-group-2"
+        label="Title"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
           v-model="todo.title"
-        >
-      </div>
-    
-      <div>
-        <label> Description </label>
-        <textarea v-model="todo.description" />
-      </div>
+          required
+          placeholder="Enter title"
+        />
+      </b-form-group>
 
-      <div>
-        <label> Category </label>
-        <select v-model="todo.category">
+      <b-form-group
+        id="input-group-2"
+        label="Description"
+        label-for="input-2"
+      >
+        <b-form-textarea
+          id="textarea"
+          v-model="todo.description"
+          placeholder="Enter description..."
+          rows="3"
+          max-rows="6"
+          required
+        />
+      </b-form-group>
+      <b-form-group
+        id="input-group-3"
+        label="Categories"
+        label-for="input-3"
+      >
+        <b-form-select v-model="todo.category" required>
           <option disabled>
             Choose
           </option>
@@ -29,21 +46,31 @@
           >
             {{ category.name }}
           </option>
-        </select>
-      </div>
-
-      <div>
-        <label>Due date</label>
-        <input
-          type="date"
+        </b-form-select>
+      </b-form-group>
+      <b-form-group
+        id="input-group-4"
+        label="État"
+        label-for="input-4"
+      >
+        <b-form-input
+          id="input-4"
           v-model="todo.date"
+          placeholder="due date"
+          type="date"
+          required
+        /> 
+      </b-form-group>
+      <b-form-group>
+        <b-button
+          type="button"
+          variant="primary"
+          @click="create"
         >
-      </div>
-    
-      <button @click="create">
-        Create
-      </button>
-    </form>
+          Submit
+        </b-button>
+      </b-form-group>
+    </b-form>
   </div>
 </template>
 
